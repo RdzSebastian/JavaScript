16eeeda02dd6864859d7c61161a90f0696681758
rodriguezsebastian <rodriguezsebastian@conicet.gov.ar>
2019-05-07 15:52:11 -0300

    Almost finish funciones

--- Curso Youtube/js/funciones.js
+++ Curso Youtube/js/funciones.js
@@ -6,13 +6,50 @@ function uno(){
 	return saludo;
 };
 
+//uno(); 
+
 function dos(a,b){
 	var datos = document.write("<br> mi nombre es: " + a + "<br> la hora es: " + b + "hrs");
 	return datos;
 }
 
-
-uno();
-
+/*
 hora = new Date();
 dos("sebastian", hora.getHours() + ":" + hora.getMinutes());
+*/
+
+function existencia(nombre, apellido){
+	if(nombre != null && apellido != null){
+		respuesta = document.write(nombre + " ", apellido);
+	}else{
+		respuesta = document.write("No puso sus datos completos");
+	};
+	return respuesta;
+}
+
+
+// existencia("Sebastian", "Rodriguez");
+
+function UsoArray(...meses){
+	for(let uno of meses){
+		document.write(uno + "<br>");
+	}
+};
+
+//UsoArray("Enero", "Febrero", "Marzo");
+
+function UsoArray2(nombre, precio){
+	return "Nombre: " + nombre + "<br>Precio: " + precio;
+}
+
+/*
+var juguete = ["Autito", 25];
+document.write( UsoArray2(...juguete));
+*/
+
+var siete = function(){ 
+	respuesta = "Hola a todos";
+	return respuesta;
+};
+
+document.write(siete());
